00ae01cdc9c364b0298cb7151fbaf34438f94bbd
Maxim Dmitriev <Renissance@tut.by>
2023-06-19 09:39:37 +0300

    refactor + add environment + catch err

--- src/app/todolists-list/todolists-lists.component.ts
+++ src/app/todolists-list/todolists-lists.component.ts
@@ -1,26 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-
-interface renderNg {
-  name: string
-  age: number
-}
-
-interface Todo {
-  addedDate: string
-  id: string
-  order: number
-  title: string
-}
-
-export interface Response {
-  data: {
-    item: Todo
-  }
-  messages: string[]
-  fieldsErrors: string[]
-  resultCode: number
-}
+import { Response, Todo, TodosService } from 'src/app/services/todos.service'
+import { HttpErrorResponse } from '@angular/common/http'
 
 @Component({
   selector: 'todo-todolists-list',
@@ -31,60 +11,38 @@ export class TodolistsListsComponent implements OnInit {
   children = 'children works'
   str = ''
 
-  renderNg: renderNg[] = [
-    { name: 'Max', age: 28 },
-    { name: 'Vasia', age: 25 },
-  ]
-  httpOptions = {
-    withCredentials: true,
-    headers: {
-      'api-key': 'fab19197-098e-4362-876a-0c0797e21ac6',
-    },
-  }
   todos: Todo[] = []
 
-  constructor(private http: HttpClient) {}
-  ngOnInit() {
-    this.getTodos()
-  }
-
-  getTodos() {
-    this.http
-      .get<Todo[]>(
-        'https://social-network.samuraijs.com/api/1.1/todo-lists',
-        this.httpOptions
-      )
-      .subscribe(res => {
-        this.todos = res
-      })
-  }
-
-  createTodo() {
-    this.http
-      .post<Response>(
-        'https://social-network.samuraijs.com/api/1.1/todo-lists',
-        {
-          title: 'angular Todo',
-        },
-        this.httpOptions
-      )
-      .subscribe(res => {
-        this.todos.unshift(res.data.item)
-      })
-  }
+  constructor(private todoService: TodosService) {}
 
   emitHandler(value: string) {
     this.str = value
   }
 
-  deleteTodo(todolistId: string) {
-    this.http
-      .delete(
-        `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
-        this.httpOptions
-      )
-      .subscribe(res => {
-        this.todos = this.todos.filter(tl => tl.id !== todolistId)
-      })
+  ngOnInit() {
+    this.getTodos()
+  }
+
+  getTodos() {
+    this.todoService.getTodos().subscribe({
+      next: res => {
+        this.todos = res
+      },
+      error: (e: HttpErrorResponse) => {
+        this.str = e.message
+      },
+    })
+  }
+
+  createTodo() {
+    this.todoService.createTodo().subscribe((res: Response) => {
+      this.todos.unshift(res.data.item)
+    })
+  }
+
+  deleteTodo(event: any, todolistId: string) {
+    this.todoService.deleteTodo(todolistId).subscribe(res => {
+      this.todos = this.todos.filter(tl => tl.id !== todolistId)
+    })
   }
 }
